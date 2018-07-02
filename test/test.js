cdc83fbe072fcf051bd85f5b6857a96f145ca3e1
asd2766 <270092797@qq.com>
2018-07-02 11:48:24 +0800

    接口调整

--- test/test.js
+++ test/test.js
@@ -53,7 +53,7 @@ describe('实现一个基类，可以继承，可以监听事件', function () {
       var myclass = new MyClass()
       // console.log(myclass)
       assert.equal(myclass.getVal(), 'hello world')
-      assert.equal(myclass.say('haha'), 'haha')
+      assert.equal(MyClass.say('haha'), 'haha')
       assert.equal(myclass instanceof MyClass, true)
       assert.equal(myclass instanceof Base, true)
     })
