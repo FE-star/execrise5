d8ed46768cb80073d484e62d259fe050009c63a4
Chen Xudong(uidp5344) <Xudong.Chen@desay-svautomotive.com>
2018-11-09 20:39:33 +0800

    test passed ——Simple

--- test/test.js
+++ test/test.js
@@ -34,6 +34,7 @@ describe('实现一个基类，可以继承，可以监听事件', function () {
       })
       view.trigger('test')
     })
+
   })
 
   describe('ES5', function () {
@@ -46,10 +47,10 @@ describe('实现一个基类，可以继承，可以监听事件', function () {
           return 'hello world'
         }
       }, {
-        say: function (word) {
-          return word
-        }
-      })
+          say: function (word) {
+            return word
+          }
+        })
       var myclass = new MyClass
       assert.equal(myclass.getVal(), 'hello world')
       assert.equal(MyClass.say('haha'), 'haha')
